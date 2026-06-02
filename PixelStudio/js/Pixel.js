39c16dd9209dac9fcb7c09efdbc4fd6151af48a3
brandonE17 <6044720@mborijnland.nl>
2026-06-02 11:07:54 +0200

    pixelclass

---
+++ PixelStudio/js/Pixel.js
@@ -0,0 +1,20 @@
+export default class Pixel {
+  constructor(x, y, size, color = '#ffffff') {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(this.x, this.y, this.size, this.size);
+  }
+
+  setColor(color) {
+    this.color = color;
+  }
+}
---
+++ PixelStudio/js/Tool.js
@@ -0,0 +1,9 @@
+export default class Tool {
+  constructor(grid) {
+    this.grid = grid;
+  }
+
+  paint(x, y, color) {
+    this.grid.setColor(x, y, color);
+  }
+}
